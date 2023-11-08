18e5b876572b627d57167a0b24f26a2f72285d63
Muhammad Mutasim Billah <130433248+mbahnaf@users.noreply.github.com>
2023-11-08 18:44:15 +0600

    Update playbook_script.sh

--- playbook_script.sh
+++ playbook_script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ansible-playbook -i inventory.ini cleanup.yml
+
 ansible-playbook -i inventory.ini dependencies.yaml
 ansible-playbook -i inventory.ini master.yaml
 ansible-playbook -i inventory.ini worker.yaml
